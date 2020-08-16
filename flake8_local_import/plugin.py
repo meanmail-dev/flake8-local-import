18cffdb6f875679157462d5da1ef3df0f0cf4d0c
meanmail <meanmail@meanmail.ru>
2020-08-16 19:53:15 +0700

    Disallow external and builtin modules in local import

--- flake8_local_import/plugin.py
+++ flake8_local_import/plugin.py
@@ -1,7 +1,12 @@
-from _ast import Import, Name
-from typing import Any
+import argparse
+import ast
+from typing import Any, List, Optional, Union
 
+from flake8.options.manager import OptionManager
 from flake8_plugin_utils import Error, Plugin, Visitor
+from flake8_plugin_utils.plugin import TConfig
+
+from flake8_local_import.constants import BUILTIN_MODULE_NAMES
 
 
 class LocalImportBeginningMethodBodyPluginError(Error):
@@ -9,17 +14,87 @@ class LocalImportBeginningMethodBodyPluginError(Error):
     message = 'Local import must be at the beginning of the method body'
 
 
+class ExternalPackagesDisallowedPluginError(Error):
+    code = 'LI101'
+    message = 'Packages from external modules should not be imported locally'
+
+
+class BuiltinModulesDisallowedPluginError(Error):
+    code = 'LI102'
+    message = 'Packages from standard modules should not be imported locally'
+
+
 class LocalImportPluginVisitor(Visitor):
-    def visit_Import(self, node: Import) -> Any:
-        if (isinstance(node.func, Name) and
-            node.func.id == 'super' and
-            node.args):
+    def visit(self, node: ast.AST) -> Any:
+        previous = None
+        for child in ast.iter_child_nodes(node):
+            child.parent = node
+            child.previous = previous
+            previous = child
+
+        return super().visit(node)
+
+    def visit_Import(self, node: ast.Import) -> Any:
+        for name in node.names:
+            self.assert_external_module(node, name.name)
+
+        return self.visit_import(node)
+
+    def assert_external_module(self, node: ast.stmt, module: str) -> None:
+        module_prefix = module + '.'
+
+        is_app_module = any(module_prefix.startswith(app_module + '.')
+                            for app_module in self.config.application_import_names)
+        is_builtin_module = module.split('.')[0] in BUILTIN_MODULE_NAMES
+
+        if not is_app_module:
+            if not is_builtin_module:
+                self.error_from_node(
+                    ExternalPackagesDisallowedPluginError, node
+                )
+            else:
+                self.error_from_node(
+                    BuiltinModulesDisallowedPluginError, node
+                )
+
+    def visit_ImportFrom(self, node: ast.ImportFrom) -> Any:
+        self.assert_external_module(node, node.module)
+
+        return self.visit_import(node)
+
+    def visit_import(self, node: Union[ast.Import, ast.ImportFrom]) -> Any:
+        parent = getattr(node, 'parent', None)
+        previous = getattr(node, 'previous', None)
+
+        if (not isinstance(parent, ast.FunctionDef) or
+            not isinstance(previous, (ast.Import, ast.ImportFrom, ast.arguments))):
             self.error_from_node(LocalImportBeginningMethodBodyPluginError, node)
 
         return self.generic_visit(node)
+
+
+class LocalImportPluginConfig:
+    def __init__(self, application_import_names: List[str]):
+        self.application_import_names = application_import_names
 
 
 class LocalImportPlugin(Plugin):
     name = 'flake8_local_import'
     version = '1.0.0'
     visitors = [LocalImportPluginVisitor]
+
+    @classmethod
+    def add_options(cls, options_manager: OptionManager):
+        options_manager.add_option(
+            '--application-import-names',
+            default='',
+            type=str
+        )
+
+    @classmethod
+    def parse_options_to_config(cls, option_manager: OptionManager,
+                                options: argparse.Namespace,
+                                args: List[str]) -> Optional[TConfig]:
+        return LocalImportPluginConfig(
+            application_import_names=options.application_import_names.split(',')
+        )
--- tests/tests.py
+++ tests/tests.py
@@ -1,23 +1,60 @@
+import pytest
 from flake8_plugin_utils import assert_error, assert_not_error
 
 from flake8_local_import.plugin import (
-    LocalImportBeginningMethodBodyPluginError, LocalImportPluginVisitor
+    BuiltinModulesDisallowedPluginError,
+    ExternalPackagesDisallowedPluginError,
+    LocalImportBeginningMethodBodyPluginError,
+    LocalImportPluginConfig, LocalImportPluginVisitor
 )
 
-code_with_error = """
-def func():
-    statement
 
-    from some_package import A
-
-"""
-
-
-def test_code_with_error():
+@pytest.mark.parametrize('code', [
+    pytest.param(
+        """
+        def func():
+            statement
+        
+            from some_package.module import A
+        """,
+        id='Import from after statement'
+    ),
+    pytest.param(
+        """
+        def func():
+            statement
+    
+            import some_package
+        """,
+        id='Import after statement'
+    ),
+    pytest.param(
+        """
+        def func():
+            statement
+        
+            if a > 1:
+                from some_package.module import A
+        """,
+        id='Import from inside statement'
+    ),
+    pytest.param(
+        """
+        def func():
+            statement
+        
+            if a > 1:
+                import some_package
+        """,
+        id='Import inside statement'
+    )
+])
+def test_code_with_error(code: str):
     assert_error(
         LocalImportPluginVisitor,
-        code_with_error,
-        LocalImportBeginningMethodBodyPluginError
+        code,
+        LocalImportBeginningMethodBodyPluginError,
+        config=LocalImportPluginConfig(application_import_names=['some_package'])
     )
 
 
@@ -30,4 +67,58 @@ def func():
 
 
 def test_code_without_error():
-    assert_not_error(LocalImportPluginVisitor, code_without_error)
+    assert_not_error(
+        LocalImportPluginVisitor,
+        code_without_error,
+        config=LocalImportPluginConfig(application_import_names=['some_package'])
+    )
+
+
+@pytest.mark.parametrize('code', [
+    pytest.param(
+        """
+        def func():
+            from external_package.module import A
+        """,
+        id='Import from external package'
+    ),
+    pytest.param(
+        """
+        def func():
+            import external_package
+        """,
+        id='Import external package'
+    )
+])
+def test_code_external_packages_disallowed(code: str):
+    assert_error(
+        LocalImportPluginVisitor,
+        code,
+        ExternalPackagesDisallowedPluginError,
+        config=LocalImportPluginConfig(application_import_names=['some_package'])
+    )
+
+
+@pytest.mark.parametrize('code', [
+    pytest.param(
+        """
+        def func():
+            from sys import float_info
+        """,
+        id='Import from builtin module'
+    ),
+    pytest.param(
+        """
+        def func():
+            import os
+        """,
+        id='Import builtin module'
+    )
+])
+def test_code_builtin_modules_disallowed(code: str):
+    assert_error(
+        LocalImportPluginVisitor,
+        code,
+        BuiltinModulesDisallowedPluginError,
+        config=LocalImportPluginConfig(application_import_names=['some_package'])
+    )
